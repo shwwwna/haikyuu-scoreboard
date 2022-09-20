56af4c0d035df4dd53ba0599ab6be0a3ad8498ce
shwwwna <107602771+shwwwna@users.noreply.github.com>
2022-09-20 22:05:01 +0800

    Upload

---
+++ index.js
@@ -0,0 +1,44 @@
+let countK = document.getElementById("count-k");
+let ctK = 0;
+
+function kOne() {
+  ctK += 1;
+  countK.textContent = ctK;
+}
+function kTwo() {
+  ctK += 2;
+  countK.textContent = ctK;
+}
+function kThree() {
+  ctK += 3;
+  countK.textContent = ctK;
+}
+
+let countS = document.getElementById("count-s");
+let ctS = 0;
+
+function sOne() {
+  ctS += 1;
+  countS.textContent = ctS;
+}
+function sTwo() {
+  ctS += 2;
+  countS.textContent = ctS;
+}
+function sThree() {
+  ctS += 3;
+  countS.textContent = ctS;
+}
+
+//
+
+//
+
+// let lightK = document.getElementById("light-k");
+// let lightS = document.getElementById("light-s");
+
+// if (ctK > ctS) {
+//   lightK.style.backgroundColor = "#fff";
+// } else {
+//   lightK.style.backgroundColor = "#000";
+// }
